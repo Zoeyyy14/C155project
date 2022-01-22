058fb003df621e3151b35ef5ca3211235e805141
Zoeyyy14 <70824285+Zoeyyy14@users.noreply.github.com>
2022-01-22 11:34:10 -0600

    Add files via upload

---
+++ Component/Game.js
@@ -0,0 +1,16 @@
+AFRAME .registerComponent("game-play",{
+    schema:{
+        elementId:{type:"string",default:"#ring1"}
+    },
+    update:function(){
+        this.isCollided(this.data.elementId)
+    },
+    isCollided:function(elementId){
+        const element=document.querySelector(elementId)
+        element.addEventListener("collide",e=>{
+            if(elementId.includes("#fish")){
+                console.log(elementId+" collision")
+            }
+        })
+    }
+})
